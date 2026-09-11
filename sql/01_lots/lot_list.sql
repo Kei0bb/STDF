f3049e4657a3e24b7980550bd27b2198020baa73
user <user@name.com>
2026-09-11 14:53:32 +0900

    sql update

--- sql/01_lots/lot_list.sql
+++ sql/01_lots/lot_list.sql
@@ -1,6 +1,9 @@
 -- ロット一覧(新しい順)。job_mixed はロット途中でテストプログラムが変わった印。
+SET VARIABLE product = '';
+
 SELECT lot_id, product, test_category, sub_process,
        start_time, finish_time,
        job_name, job_rev, job_variant_count, job_mixed
 FROM lots
+WHERE product = getvariable('product')
 ORDER BY start_time DESC
